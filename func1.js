a0ce72bbf865b7038f914b1dfcf28394e74aa4f6
andreroble <152176042+Andre-Roble@users.noreply.github.com>
2024-09-11 02:44:18 +0800

    added SSL checker 9/11

--- func1.js
+++ func1.js
@@ -31,3 +31,138 @@ blockAdTrackersCheckbox.addEventListener('change', () => {
   }
 });
 
+//back button
+
+document.querySelector('.back-button').addEventListener('click', () => {
+  // Add your logic here to go back to the previous page
+  window.history.back();
+});
+
+const checkButton = document.querySelector('.check-btn');
+checkButton.addEventListener('click', fetchData);
+
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  var currentTab = tabs[0];
+  var url = currentTab.url;
+  var domain = url.replace(/https?:\/\//, ''); // for auto
+  document.getElementById('textInput').value = domain;
+});
+
+async function fetchData() {
+  const textInput = document.getElementById('textInput').value.trim();
+  if (!textInput) {
+    alert('Please enter a valid domain');
+    return;
+  }
+
+  const domain = textInput.replace(/https?:\/\//, ''); // for input
+  const parts = domain.split('.');
+  if (parts.length < 2) {
+    alert('Invalid input. Please enter a valid domain (e.g. example.com, etc.)');
+    return;
+  }
+
+  try {
+    const response = await fetch(`https://check-ssl.p.rapidapi.com/sslcheck?domain=${domain}`, {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '745c95af6bmshe6b922b175f24bdp1921e4jsn7694b614c269',
+        'x-rapidapi-host': 'check-ssl.p.rapidapi.com'
+      }
+    });
+    const data = await response.json();
+    displayResults(data);
+  } catch (error) {
+    console.error(error);
+    displayError('Failed to fetch data. Please try again later.');
+  }
+}
+
+
+function displayResults(data) {
+  const resultElement = document.getElementById('ssl-result');
+  const keysToDisplay = [
+    'isvalidCertificate',
+    'canBeSelfSigned',
+    'isWildCard',
+    'isExpired',
+    'message',
+    'expiry',
+    'daysLeft',
+    'lifespanInDays',
+    'issuer',
+  ];
+
+  const keysToDisplay1 = [
+    'certDetails',
+    'name',
+    'CN',
+    'hash',
+    'issuer',
+    'C',
+    'O',
+    'version',
+    'serialNumber',
+    'serialNumberHex',
+    'validFrom',
+    'validTo',
+    'validFrom_time_t',
+    'validTo_time_t',
+    'signatureTypeSN',
+    'signatureTypeLN',
+    'signatureTypeNID',
+    'validLeft',
+    'apiVersion',
+    'error'
+  ];
+
+  const displayData = (data, keys) => {
+    return keys.map(key => {
+      if (keysToDisplay.includes(key) || keysToDisplay1.includes(key)) {
+        if (typeof data[key] === 'object') {
+          return `<tr><td colspan="2">${key}</td></tr>${displayData(data[key], Object.keys(data[key]))}`;
+        } else if (data[key] !== undefined) {
+          return `<tr><td>${key}</td><td>${data[key]}</td></tr>`;
+        } else {
+          return '';
+        }
+      }
+      return '';
+    }).join('');
+  };
+
+  const tableTemplate = (data, keys) => {
+    return `
+      <table class="ssl-result-table">
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${displayData(data, keys)}
+        </tbody>
+      </table>
+    `;
+  };
+
+  const showMoreButton = document.createElement('button');
+  showMoreButton.textContent = '--Show more--';
+  showMoreButton.classList.add('show-more-button'); // add a class to the button
+  showMoreButton.onclick = () => {
+    resultElement.innerHTML = tableTemplate(data, [...keysToDisplay, ...keysToDisplay1]);
+    const showLessButton = document.createElement('button');
+    showLessButton.textContent = '--Show less--';
+    showLessButton.classList.add('show-less-button'); // add a class to the button
+    showLessButton.onclick = () => {
+      resultElement.innerHTML = tableTemplate(data, keysToDisplay);
+      resultElement.appendChild(showMoreButton);
+    };
+    resultElement.appendChild(showLessButton);
+  };
+
+  resultElement.innerHTML = tableTemplate(data, keysToDisplay);
+  resultElement.appendChild(showMoreButton);
+}
--- ssl_func.py
+++
@@ -1,9 +0,0 @@
-from ssl_checker import SSLChecker
-
-SSLChecker = SSLChecker()
-args = {
-    'hosts': ['google.com', 'cisco.com']
-}
-
-SSLChecker.show_result(SSLChecker.get_args(json_args=args))
-
